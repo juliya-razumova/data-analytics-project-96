a45a81632962e5e479f713bf297ac47e8e9f76d7
juliya-razumova <131293987+juliya-razumova@users.noreply.github.com>
2023-09-08 16:10:54 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,69 +1,84 @@
 with tab as (
-select 
-visitor_id,
-max(visit_date) as visit_date
-from sessions s 
-where campaign is not null
-group by visitor_id
+    select
+        visitor_id,
+        max(visit_date) as visit_date
+    from sessions
+    where campaign is not null
+    group by visitor_id
 ),
+
 ya as (
-select 
-cast(campaign_date as date) as campaign_date,
-utm_source,
-utm_medium,
-utm_campaign,
-sum(daily_spent) as total_cost
-from ya_ads
-group by campaign_date, utm_source, utm_medium, utm_campaign
+    select
+        cast(campaign_date as date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by campaign_date, utm_source, utm_medium, utm_campaign
 ),
+
 vk as (
-select 
-cast(campaign_date as date) as campaign_date,
-utm_source,
-utm_medium,
-utm_campaign,
-sum(daily_spent) as total_cost
-from vk_ads
-group by campaign_date, utm_source, utm_medium, utm_campaign
+    select
+        cast(campaign_date as date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from vk_ads
+    group by campaign_date, utm_source, utm_medium, utm_campaign
 ),
+
 tab1 as (
-select 
-cast (tab.visit_date as date) as date,
-source,
-medium,
-campaign,
-count(distinct tab.visitor_id) as visitors_count,
-count(lead_id) filter (where created_at > tab.visit_date) as leads_count,
-count(lead_id) filter (where amount > 0) as purchases_count,
-sum(amount) as revenue
-from tab
-left join sessions
-on tab.visitor_id = sessions.visitor_id and tab.visit_date = sessions.visit_date
-left join leads
-on tab.visitor_id = leads.visitor_id
-group by date, source, medium, campaign
+    select
+        cast(tab.visit_date as date) as date,
+        source,
+        medium,
+        campaign,
+        count(distinct tab.visitor_id) as visitors_count,
+        count(lead_id) filter (
+            where created_at >= tab.visit_date
+        ) as leads_count,
+        count(lead_id) filter (where amount > 0) as purchases_count,
+        sum(amount) as revenue
+    from tab
+    left join sessions
+        on
+            tab.visitor_id = sessions.visitor_id
+            and tab.visit_date = sessions.visit_date
+    left join leads
+        on tab.visitor_id = leads.visitor_id
+    group by date, source, medium, campaign
 )
-select 
-tab1.date as visit_date,
-tab1.visitors_count,
-tab1.source as utm_source,
-tab1.medium as utm_medium,
-tab1.campaign as utm_campaign,
-case 
-	when source = 'yandex' then ya.total_cost
-	when source = 'vk' then vk.total_cost
-end as total_cost,
-tab1.leads_count,
-tab1.purchases_count,
-tab1.revenue
+
+select
+    tab1.date as visit_date,
+    tab1.visitors_count,
+    tab1.source as utm_source,
+    tab1.medium as utm_medium,
+    tab1.campaign as utm_campaign,
+    tab1.leads_count,
+    tab1.purchases_count,
+    tab1.revenue,
+    case
+        when source = 'yandex' then ya.total_cost
+        when source = 'vk' then vk.total_cost
+    end as total_cost
 from tab1
 left join ya
-on date = ya.campaign_date and source = ya.utm_source
-and medium = ya.utm_medium and campaign = ya.utm_campaign
+    on
+        date = ya.campaign_date and source = ya.utm_source
+        and medium = ya.utm_medium and campaign = ya.utm_campaign
 left join vk
-on date = vk.campaign_date and source = vk.utm_source
-and medium = vk.utm_medium and campaign = vk.utm_campaign
-where source in ('vk', 'yandex')
-order by revenue desc nulls last, visit_date, visitors_count desc, utm_source, utm_medium, utm_campaign
+    on
+        date = vk.campaign_date and source = vk.utm_source
+        and medium = vk.utm_medium and campaign = vk.utm_campaign
+where source != 'admitad'
+order by
+    revenue desc nulls last,
+    visitors_count desc,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
 limit 15;
-
