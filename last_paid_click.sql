98ffa7d88ddd706f43875c9d1661a4f538cb2a4a
juliya-razumova <131293987+juliya-razumova@users.noreply.github.com>
2023-09-08 16:11:46 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,26 +1,29 @@
 with tab as (
-select 
-visitor_id,
-max(visit_date) as visit_date
-from sessions s 
-where campaign is not null
-group by visitor_id
+    select
+        visitor_id,
+        max(visit_date) as visit_date
+    from sessions
+    where campaign is not null
+    group by visitor_id
 )
+
 select
-tab.visitor_id,
-tab.visit_date,
-source as utm_source,
-medium as utm_medium,
-campaign as utm_campaign,
-lead_id,
-created_at,
-amount,
-closing_reason,
-status_id
+    tab.visitor_id,
+    tab.visit_date,
+    source as utm_source,
+    medium as utm_medium,
+    campaign as utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
 from tab
 left join sessions
-on tab.visitor_id = sessions.visitor_id and tab.visit_date = sessions.visit_date
+    on
+        tab.visitor_id = sessions.visitor_id
+        and tab.visit_date = sessions.visit_date
 left join leads
-on tab.visitor_id = leads.visitor_id
-order by amount desc nulls last, visit_date, utm_source, utm_medium, utm_campaign;
-
+    on tab.visitor_id = leads.visitor_id
+order by
+    amount desc nulls last, visit_date asc, utm_source asc, utm_medium asc, utm_campaign asc;
