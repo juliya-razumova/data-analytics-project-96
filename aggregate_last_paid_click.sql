483c18d16cec2a8657bfbfd1afa6aacd2d64e818
juliya-razumova <131293987+juliya-razumova@users.noreply.github.com>
2023-09-10 19:31:40 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,10 +1,10 @@
 with tab as (
-    select
-        visitor_id,
-        max(visit_date) as visit_date
-    from sessions
-    where campaign is not null
-    group by visitor_id
+select
+    visitor_id,
+    max(visit_date) as visit_date
+from sessions
+where campaign is not null
+group by visitor_id
 ),
 
 ya as (
