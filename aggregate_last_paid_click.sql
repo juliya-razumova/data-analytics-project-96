db93ae3c765e6cb1c7009a9b3436fc54cec45a90
juliya-razumova <131293987+juliya-razumova@users.noreply.github.com>
2023-09-11 15:10:02 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -43,15 +43,16 @@ display as (
             where leads.created_at >= tab.visit_date and leads.amount > 0
         ) as purchases_count,
         sum(leads.amount) filter (
-                where leads.created_at >= tab.visit_date) as revenue
+            where leads.created_at >= tab.visit_date
+        ) as revenue
     from tab
     left join sessions
-        on 
+        on
             tab.visitor_id = sessions.visitor_id
-            tab.visit_date = sessions.visit_date
+            and tab.visit_date = sessions.visit_date
     left join leads
         on tab.visitor_id = leads.visitor_id
-    group by 
+    group by
         cast(tab.visit_date as date),
         sessions.source,
         sessions.medium,
@@ -64,32 +65,30 @@ select
     display.utm_source,
     display.utm_medium,
     display.utm_campaign,
+    display.leads_count,
+    display.purchases_count,
+    display.revenue,
     case
         when display.utm_source = 'yandex' then ya.total_cost
         when display.utm_source = 'vk' then vk.total_cost
-    end as total_cost,
-    display.leads_count,
-    display.purchases_count,
-    display.revenue
+    end as total_cost
 from display
 left join ya
     on
         display.visit_date = ya.visit_date
-        display.utm_source = ya.utm_source
-        display.utm_medium = ya.utm_medium
-        display.utm_campaign = ya.utm_campaign
+        and display.utm_source = ya.utm_source
+        and display.utm_medium = ya.utm_medium
+        and display.utm_campaign = ya.utm_campaign
 left join vk
     on
         display.visit_date = vk.visit_date
-        display.utm_source = vk.utm_source
-        display.utm_medium = vk.utm_medium
-        display.utm_campaign = vk.utm_campaign
+        and display.utm_source = vk.utm_source
+        and display.utm_medium = vk.utm_medium
+        and display.utm_campaign = vk.utm_campaign
 order by
     display.revenue desc nulls last,
-    display.visit_date,
+    display.visit_date asc,
     display.visitors_count desc,
-    display.utm_source,
-    display.utm_medium,
-    display.utm_campaign,
-    display.leads_count,
-    display.purchases_count;
+    display.utm_source asc,
+    display.utm_medium asc,
+    display.utm_campaign asc;
