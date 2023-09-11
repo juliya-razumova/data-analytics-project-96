c3383026c9e421a5b91929901786abb253e070b2
juliya-razumova <131293987+juliya-razumova@users.noreply.github.com>
2023-09-11 14:30:14 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -7,7 +7,7 @@ with tab as (
     group by visitor_id
 ),
 
-    ya as (
+ya as (
     select
         cast(campaign_date as date) as visit_date,
         utm_source,
@@ -18,7 +18,7 @@ with tab as (
     group by campaign_date, utm_source, utm_medium, utm_campaign
 ),
 
-    vk as (
+vk as (
     select
         cast(campaign_date as date) as visit_date,
         utm_source,
@@ -29,55 +29,57 @@ with tab as (
     group by campaign_date, utm_source, utm_medium, utm_campaign
 ),
 
-    display as (
+display as (
     select
         cast(tab.visit_date as date) as visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
+        sessions.source as utm_source,
+        sessions.medium as utm_medium,
+        sessions.campaign as utm_campaign,
         count(distinct tab.visitor_id) as visitors_count,
-        count(l.lead_id) filter (
-            where l.created_at >= tab.visit_date
+        count(leads.lead_id) filter (
+            where leads.created_at >= tab.visit_date
         ) as leads_count,
-        count(l.lead_id) filter (
-            where l.created_at >= tab.visit_date and l.amount > 0
+        count(leads.lead_id) filter (
+            where leads.created_at >= tab.visit_date and leads.amount > 0
         ) as purchases_count,
-        sum(l.amount) filter (where l.created_at >= tab.visit_date) as revenue
+        sum(leads.amount) filter (where leads.created_at >= tab.visit_date) as revenue
     from tab
-    left join sessions s
-    on tab.visitor_id = s.visitor_id and tab.visit_date = s.visit_date
-    left join leads l
-    on tab.visitor_id = l.visitor_id
-    group by cast(tab.visit_date as date), s.source, s.medium, s.campaign
+    left join sessions 
+    on tab.visitor_id = sessions.visitor_id and tab.visit_date = sessions.visit_date
+    left join leads
+    on tab.visitor_id = leads.visitor_id
+    group by cast(tab.visit_date as date), sessions.source, sessions.medium, sessions.campaign
 )
 select
-    d.visit_date,
-    d.visitors_count,
-    d.utm_source,
-    d.utm_medium,
-    d.utm_campaign,
+    display.visit_date,
+    display.visitors_count,
+    display.utm_source,
+    display.utm_medium,
+    display.utm_campaign,
     case
-        when d.utm_source = 'yandex' then y.total_cost
-        when d.utm_source = 'vk' then v.total_cost
+        when display.utm_source = 'yandex' then ya.total_cost
+        when display.utm_source = 'vk' then vk.total_cost
     end as total_cost,
-    d.leads_count,
-    d.purchases_count,
-    d.revenue
-from display d
-left join ya y
-    on d.visit_date = y.visit_date and
-        d.utm_source = y.utm_source and
-        d.utm_medium = y.utm_medium and
-        d.utm_campaign = y.utm_campaign
-left join vk as v
-    on d.visit_date = v.visit_date and
-        d.utm_source = v.utm_source and
-        d.utm_medium = v.utm_medium and
-        d.utm_campaign = v.utm_campaign
+    display.leads_count,
+    display.purchases_count,
+    display.revenue
+from display
+left join ya
+    on
+        display.visit_date = ya.visit_date
+        display.utm_source = ya.utm_source
+        display.utm_medium = ya.utm_medium
+        display.utm_campaign = ya.utm_campaign
+left join vk
+    on
+        display.visit_date = vk.visit_date
+        display.utm_source = vk.utm_source
+        display.utm_medium = vk.utm_medium
+        display.utm_campaign = vk.utm_campaign
 order by 
-    d.revenue desc nulls last, 
-    d.visit_date, 
-    d.visitors_count desc, 
-    d.utm_source, 
-    d.utm_medium, 
-    d.utm_campaign;
+    display.revenue desc nulls last, 
+    display.visit_date, 
+    display.visitors_count desc, 
+    display.utm_source, 
+    display.utm_medium, 
+    display.utm_campaign;
